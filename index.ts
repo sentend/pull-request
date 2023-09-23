bef7b8238e6fc7bb35e331f83d2056842fff8a0b
senzu <wecken2222@yandex.ru>
2023-09-23 20:14:58 +0700

    some stuff in TEST not upstream branch

--- index.ts
+++ index.ts
@@ -4,3 +4,5 @@ console.log('checkout to bracnh feature')
 
 console.log('new clg wrote in main branch')
 console.log('new clg wrote in main branch 2')
+
+console.log('some stuff in TEST not upstream branch')
